91734ecfd3069dd8b44ad78e2a3a981ae5c0d7a2
Marko Doda <marko@lugola.net>
2015-07-06 00:57:19 +0200

    Wiki bot now uses tabela as a function.

--- wiki.py
+++ wiki.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 from simplemediawiki import MediaWiki
+from tabela import tabela
+from people import people
 import sys
 text = '==== Листа на дежурства ====\n\nОва е автоматски генерерирана листа на дежурни со две ротации, доколку не сте во можност да бидете дежурни некоја недела или ден запишете во забелешка и пишете на мејлинг листа. Доколку сте дежурен во вашиот google calendar е вметнат нов календар насловен „Хаклаб: Дежурства“ со настан за деновите кога сте дежурни. Поставете ги известувањата за да бидете навреме известени.\n\n'
-text+=sys.stdin.read()
+text+=tabela(people)
 wiki = MediaWiki('https://wiki.spodeli.org/api.php')
 user, password = open('credentials', 'r').read().split()
 wiki.login(user,password)
